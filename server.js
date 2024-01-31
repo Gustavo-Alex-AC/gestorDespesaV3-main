0fcc348ebdb3ac96c872401f6cb7ca3ec2afe4ce
Gustavo Chitangua <gchitangua@gmail.com>
2024-01-31 21:58:56 +0100

    firstcommitLenovo

---
+++ server.js
@@ -0,0 +1,269 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { Pool } = require("pg");
+const crypto = require("crypto");
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(cors());
+const secretKey = "g3stor@D3sp3s@s*";
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "1234",
+  port: 5432,
+});
+
+// Store refresh tokens securely (e.g., in a database)
+const refreshTokens = {};
+
+// User Authentication Middleware
+const authenticateToken = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token || !token.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  const authToken = token.slice(7); // Remove 'Bearer ' from the token string
+
+  jwt.verify(authToken, secretKey, (err, user) => {
+    if (err) {
+      if (err.name === "TokenExpiredError") {
+        return res.status(401).json({ error: "Token expired" });
+      } else {
+        console.error("Token verification error:", err);
+        return res.status(403).json({ error: "Forbidden" });
+      }
+    }
+    req.user = user;
+    next();
+  });
+};
+
+// Generate Refresh Token during Sign-in
+const generateRefreshToken = () => {
+  const refreshBuffer = new Uint8Array(64);
+  crypto.getRandomValues(refreshBuffer);
+  return Array.from(refreshBuffer, (byte) => byte.toString(16)).join("");
+};
+
+// Sign-in endpoint
+app.post("/api/auth/signin", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Fetch user data from the database based on the provided username
+    const result = await pool.query(
+      "SELECT id, username, password_hash FROM users WHERE username = $1",
+      [username]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    const user = result.rows[0];
+
+    // Compare the provided password with the hashed password stored in the database
+    const passwordMatch = await bcrypt.compare(password, user.password_hash);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    // Create and send a JWT token upon successful sign-in
+    // Create and send a JWT token and refresh token upon successful sign-in
+    const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: "1h" });
+    const refreshToken = generateRefreshToken();
+    refreshTokens[username] = refreshToken;
+
+    res.json({ token, refreshToken, user });
+  } catch (error) {
+    console.error("Sign-in error", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// SignUp endpoint
+app.post("/api/auth/signup", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Check if the username is already taken
+    const existingUser = await pool.query(
+      "SELECT * FROM users WHERE username = $1",
+      [username]
+    );
+
+    if (existingUser.rows.length > 0) {
+      return res.status(400).json({ error: "Username is already taken" });
+    }
+
+    // Hash the password before storing it in the database
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Insert the new user into the database
+    const result = await pool.query(
+      "INSERT INTO users (username, password_hash) VALUES ($1, $2) RETURNING *",
+      [username, hashedPassword]
+    );
+
+    const newUser = result.rows[0];
+
+    // Create and send a JWT token and refresh token upon successful sign-in
+    // Create and send a JWT token and refresh token upon successful sign-up
+    const token = jwt.sign({ userId: newUser.id }, secretKey, {
+      expiresIn: "1h",
+    });
+    const refreshToken = generateRefreshToken();
+    refreshTokens[username] = refreshToken;
+
+    res.json({ token, refreshToken, user: newUser });
+  } catch (error) {
+    console.error("Sign-up error", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Refresh Token endpoint
+app.post("/api/auth/refresh", async (req, res) => {
+  const { refreshToken } = req.body;
+
+  // Check if the refresh token is valid
+  if (!refreshTokens[refreshToken]) {
+    return res.status(403).json({ error: "Invalid refresh token" });
+  }
+
+  // Assume the user is identified by the username stored in the refresh token
+  const username = refreshTokens[refreshToken];
+
+  // Generate a new access token
+  const newAccessToken = jwt.sign({ userId: user.id }, secretKey, {
+    expiresIn: "5h",
+  });
+
+  // Send the new access token to the client
+  res.json({ token: newAccessToken });
+
+  // You might want to remove the old refresh token to prevent multiple use
+  delete refreshTokens[refreshToken];
+});
+
+// Get all incomes for the authenticated user
+app.get("/api/incomes", authenticateToken, async (req, res) => {
+  const userId = req.user.userId;
+
+  try {
+    const result = await pool.query(
+      "SELECT * FROM incomes WHERE user_id = $1",
+      [userId]
+    );
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error fetching incomes", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Create a new income for the authenticated user
+app.post("/api/incomes", authenticateToken, async (req, res) => {
+  const { amount, category, date, reference, title } = req.body;
+  const userId = req.user.userId;
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO incomes (amount, category, date, reference, title, user_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [amount, category, date, reference, title, userId]
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error creating income", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Delete an income for the authenticated user
+app.delete("/api/incomes/:id", authenticateToken, async (req, res) => {
+  const incomeId = req.params.id;
+  const userId = req.user.userId;
+
+  try {
+    const result = await pool.query(
+      "DELETE FROM incomes WHERE id = $1 AND user_id = $2 RETURNING *",
+      [incomeId, userId]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Income not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error deleting income", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Get all expenses for the authenticated user
+app.get("/api/expenses", authenticateToken, async (req, res) => {
+  const userId = req.user.userId;
+
+  try {
+    const result = await pool.query(
+      "SELECT * FROM expenses WHERE user_id = $1",
+      [userId]
+    );
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error fetching expenses", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Create a new expense for the authenticated user
+app.post("/api/expenses", authenticateToken, async (req, res) => {
+  const { amount, category, date, reference, title } = req.body;
+  const userId = req.user.userId;
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO expenses (amount, category, date, reference, title, user_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [amount, category, date, reference, title, userId]
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error creating expense", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Delete an expense for the authenticated user
+app.delete("/api/expenses/:id", authenticateToken, async (req, res) => {
+  const expenseId = req.params.id;
+  const userId = req.user.userId;
+
+  try {
+    const result = await pool.query(
+      "DELETE FROM expenses WHERE id = $1 AND user_id = $2 RETURNING *",
+      [expenseId, userId]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Expense not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error deleting expense", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Other CRUD operations for expenses
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ serverV1.js
@@ -0,0 +1,180 @@
+const express = require("express");
+const { Pool } = require("pg");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "gestordespesa",
+  password: "1234",
+  port: 5432,
+});
+//5433
+
+app.use(express.json());
+app.use(cors());
+
+// Get all incomes
+app.get("/api/incomes", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM incomes");
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error fetching incomes", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Create a new income
+app.post("/api/incomes", async (req, res) => {
+  const { amount, category, date, reference, title, userId } = req.body;
+  try {
+    const result = await pool.query(
+      "INSERT INTO incomes (amount, category, date, reference, title, userId) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [amount, category, date, reference, title, userId]
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error creating income", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Delete an expense
+app.delete("/api/incomes/:id", async (req, res) => {
+  const incomeId = req.params.id;
+  try {
+    const result = await pool.query(
+      "DELETE FROM incomes WHERE id = $1 RETURNING *",
+      [incomeId]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Income not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error deleting income", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Get all expenses
+app.get("/api/expenses", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM expenses");
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error fetching expenses", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Create a new expense
+app.post("/api/expenses", async (req, res) => {
+  const { amount, category, date, reference, title, userId } = req.body;
+  try {
+    const result = await pool.query(
+      "INSERT INTO expenses (amount, category, date, reference, title, userId) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [amount, category, date, reference, title, userId]
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error creating expense", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Delete an expense
+app.delete("/api/expenses/:id", async (req, res) => {
+  const expenseId = req.params.id;
+  try {
+    const result = await pool.query(
+      "DELETE FROM expenses WHERE id = $1 RETURNING *",
+      [expenseId]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Expense not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error deleting expense", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Other CRUD operations for expenses
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Create and send a JWT token and refresh token upon successful sign-in
+const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: "5h" });
+const refreshToken = generateRefreshToken();
+refreshTokens[username] = refreshToken;
+
+res.json({ token, refreshToken, user });
+
+// Refresh Token endpoint
+app.post("/api/auth/refresh", async (req, res) => {
+  const { refreshToken } = req.body;
+
+  // Check if the refresh token is valid
+  if (!refreshTokens[refreshToken]) {
+    return res.status(403).json({ error: "Invalid refresh token" });
+  }
+
+  // Assume the user is identified by the username stored in the refresh token
+  const username = refreshTokens[refreshToken];
+
+  // Generate a new access token
+  const newAccessToken = jwt.sign({ userId: user.id }, secretKey, {
+    expiresIn: "5h",
+  });
+
+  // Send the new access token to the client
+  res.json({ token: newAccessToken });
+
+  // You might want to remove the old refresh token to prevent multiple use
+  delete refreshTokens[refreshToken];
+});
+
+// CREATE TABLE users (
+//   id SERIAL PRIMARY KEY,
+//   username VARCHAR(50) UNIQUE NOT NULL,
+//   password_hash VARCHAR(255) NOT NULL
+// );
+
+// -- Add user_id to incomes table
+// ALTER TABLE incomes
+// ADD COLUMN user_id INTEGER REFERENCES users(id);
+
+// -- Add user_id to expenses table
+// ALTER TABLE expenses
+// ADD COLUMN user_id INTEGER REFERENCES users(id);
+
+// -- Insert new data into the incomes table with date in 'YYYY-MM-DD' format
+// INSERT INTO incomes (amount, category, date, reference, title, user_id)
+// VALUES
+//  (1200, 'Salário', '2023-12-22', 'Mês de Janeiro', 'Salário', 1),
+//  (1000, 'Bônus', '2023-12-27', 'Oferta de desempenho', 'Bônus', 1);
+
+// -- Insert new data into the expenses table with date in 'YYYY-MM-DD' format
+// INSERT INTO expenses (amount, category, date, reference, title, user_id)
+// VALUES
+// (200, 'Alimentação', '2023-12-22', 'Comida de casa', 'Weekly Groceries', 1),
+// (150, 'Serviços Públicos', '2023-12-27', 'Pagamento a ENDE', 'Electricity Bill', 1);
+
+// INSERT INTO incomes (amount, category, date, reference, title, user_id)
+// VALUES
+//  (400, 'Outras Receitas', '2023-12-15', 'Oferta de Natal', 'Presente de Natal', 2),
+//  (100, 'Presentes', '2023-12-05', 'Tio Smith', 'Oferta', 2);
+
+// -- Insert new data into the expenses table with date in 'YYYY-MM-DD' format
+// INSERT INTO expenses (amount, category, date, reference, title, user_id)
+// VALUES
+// (50, 'Alimentação', '2023-12-15', 'Lanches da fau', 'Lanche', 2),
+// (70, 'Lazer', '2023-12-05', 'Cinema com as amigas', 'Cinema', 2);
